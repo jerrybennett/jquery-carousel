0a66247c516bff87fd2bc89173f5177608eb922e
jerrybennett <jerrybennett@users.noreply.github.com>
2015-03-19 20:23:34 -0400

    update carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,24 +1,41 @@
-$(function (){
+$(function(){
 
-	var numberSlides = $('.slide').size(),
-		slideWidth = $('.slide').width(),
-		scrollValue = 0;
+  var numberSlides = $('.slide').size();
+  var slideWidth = $('.slide').width();
+  var scrollValue = 0;
 
-	// set width of the #tray
-	$('#tray').width(numberSlides * slideWidth);
+  // set the width #tray
+  $('#tray').width(numberSlides * slideWidth);
 
-	$('#next').click(function(event){
-		event.preventDefault();
-		scrollValue += 1;
-		var position = scrollValue * slideWidth;
-		$('#tray').animate({right: position}, 400);
-	});
+  $('#next').click(function(event){
+    event.preventDefault();
+    if (scrollValue < numberSlides - 1) {
+      scrollValue += 1;
+    } else {
+      scrollValue = 0;
+    }
+    var position = scrollValue * slideWidth;
+    $('#tray').animate({right: position}, 400);
+  });
 
-	$('#prev').click(function(event){
-		event.preventDefault();
-		scrollValue -= 1;
-		var position = scrollValue * slideWidth;
-		$('#tray').animate({right: position}, 400);
-	});
+  $('#prev').click(function(event){
+    event.preventDefault();
+    if (scrollValue > 0) {
+      scrollValue -= 1;
+    } else {
+      scrollValue = numberSlides - 1;
+    }
+    var position = scrollValue * slideWidth;
+    $('#tray').animate({right: position}, 400);
+  });
+
+
+  $('#prev, #next').hide();
+
+  $('#frame').hover(function(){
+    $('#prev, #next').show();
+  }, function(){
+    $('#prev, #next').hide();
+  });
 
 });
